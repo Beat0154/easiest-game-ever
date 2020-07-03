4f4beb063d4e287334034d8edc5f8e0e79d067bc
Shawn Beaton <beat0154@algonquinlive.com>
2020-07-02 21:11:28 -0400

    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+        counter++;
+    },300);
+}
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        alert("Game Over. score: "+counter);
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }
+}, 10);
