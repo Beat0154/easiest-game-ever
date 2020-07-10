9ecd7e4029039f1236f7fe5748a5d9f2b8302de6
hatem07 <hatemghouthi@yahoo.com>
2020-07-10 11:49:26 +0100

    update
    organize function
    and add some tweaks

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+var btns=document.getElementById("btns");
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
+
+
 function jump(){
     if(character.classList == "animatech"){return}
     character.classList.add("animatech");
@@ -8,27 +11,33 @@ function jump(){
         character.classList.remove("animatech");
     },300);
 }
-function sta(i){
-document.getElementById(i).innerHTML="Jump";
- if(block.classList == "animateb"){return}block.classList.add("animateb");
-var checkDead=setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+function sta(){
+function check() {
+  let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
+if(blockLeft<20 && blockLeft>-20 && characterTop>=130){dead()}else{next()}
+}
+
+function dead(){
+  btns.style.display="block";
+block.classList.remove("animateb");
+  clearInterval(core);
+  console.log("dead");
         //alert("Game Over. score: "+Math.floor(counter/100));
         var sb=document.getElementById('scorebest');
         if(sb.innerHTML< Math.floor(counter/100)){
           sb.innerHTML= Math.floor(counter/100);
         }
         counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
-    }
-}, 10);}
-function stp(){
-var block = document.getElementById("block");
-block.classList.remove("animateb");
 }
+
+function next(){
+counter++;
+document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+}
+
+
+if(block.classList == "animateb"){return}block.classList.add("animateb");
+
+var core=setInterval(check, 10);}
